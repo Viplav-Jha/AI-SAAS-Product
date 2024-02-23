6ee30acdbc346148968be5b1226be7b5b8bae47a
Viplav jha <viplavjha1993@gmail.com>
2024-02-23 19:57:24 +0530

    files added and changes

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,13 +1,23 @@
-import { UserButton } from '@clerk/nextjs';
-import React from 'react'
+"use client"
+import React from 'react';
+
 
 const Home = () => {
+
+
   return (
-    <div className="h-screen">
-      Home
-    <UserButton afterSignOutUrl='/' />
-  </div>
-  )
-}
+    <div className="flex items-center justify-center h-screen">
+      <div className="bg-gray-100 p-8 rounded-md shadow-md" >
+        
+        <p className="text-xl font-semibold mb-4">
+          We will be launching AI image transformation very soon. Stay connected with my team.
+        </p>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
+          Like
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
