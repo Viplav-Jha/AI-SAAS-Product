b3bc88b4de2b6cdd9cd7e5240d6d0610695190eb
Viplav jha <viplavjha1993@gmail.com>
2024-02-23 19:56:34 +0530

    file cadded and changes

--- src/app/(auth)/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from 'react'
-
-const Layout = ({children}:{children :React.ReactNode}) => {
-  return (
-    <div className='auth'>{children}</div>
-  )
-}
-
-export default Layout;
