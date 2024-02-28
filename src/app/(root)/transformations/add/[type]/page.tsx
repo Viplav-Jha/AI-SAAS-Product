cc2b31b746997fca470e42f84b4158c049f21aa8
Viplav jha <viplavjha1993@gmail.com>
2024-02-28 10:43:13 +0530

    title and subtitle adde d

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { transformationTypes } from "@/constant";
 
-const AddTransformationTypePage = () => {
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+  const transformation = transformationTypes[type];
   return (
-    <div>AddTransformationTypePage</div>
-  )
-}
+   <>
+    <Header title={transformation.title} 
+    subTitle={transformation.subTitle} 
+    />
+    <TransformationForm/>
+   </>
+  );
+};
 
 export default AddTransformationTypePage;
