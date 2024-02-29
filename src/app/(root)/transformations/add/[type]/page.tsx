b6a05bfce661812ac3eb0eaf93de6151d8c430e0
Viplav jha <viplavjha1993@gmail.com>
2024-02-29 10:39:23 +0530

    intergated cloudnary

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,16 +1,31 @@
 import Header from "@/components/shared/Header";
 import TransformationForm from "@/components/shared/TransformationForm";
 import { transformationTypes } from "@/constant";
+import { getUserById } from "@/lib/action/user.action";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
   const transformation = transformationTypes[type];
+  const { userId } = auth();
+  if (!userId) return redirect('/sign-in')
+
+  // const user = await getUserById(userId);
   return (
-   <>
-    <Header title={transformation.title} 
-    subTitle={transformation.subTitle} 
-    />
-    <TransformationForm/>
-   </>
+    <>
+      <Header title={transformation.title} subTitle={transformation.subTitle} />
+      <section className="mt-10">
+      <TransformationForm
+        action="Add"
+        userId={userId}
+        type={transformation.type as TransformationTypeKey}
+        creditBalance={35}
+      />
+      </section>
+    </>
   );
 };
 
