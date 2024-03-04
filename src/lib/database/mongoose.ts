b18900730d51cba3e78d9c707a5f211b0972d4b4
Viplav jha <viplavjha1993@gmail.com>
2024-03-04 21:19:14 +0530

    indentation corrected

--- src/lib/database/mongoose.ts
+++ src/lib/database/mongoose.ts
@@ -1,8 +1,8 @@
-import mongoose, { Mongoose } from 'mongoose';
+import mongoose, { Mongoose } from "mongoose";
 
 const MONGODB_URL =
   process.env.MONGODB_URL ||
-  'mongodb+srv://viplavjha1993:PfYv1ksLaz4MNcKO@cluster0.kaxh4qe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+  "mongodb+srv://viplavjha1993:PfYv1ksLaz4MNcKO@cluster0.kaxh4qe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 interface MongooseConnection {
   conn: Mongoose | null;
@@ -21,16 +21,21 @@ if (!cached) {
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
 
-  if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+  if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
 
   // Adjusted connection options for MongoDB Atlas in the connection string
-  cached.promise = cached.promise || mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
   try {
     cached.conn = await cached.promise;
-    console.log('Connected to MongoDB Atlas successfully!');
+    console.log("Connected to MongoDB Atlas successfully!");
   } catch (error) {
-    console.error('MongoDB Atlas connection error:', error);
+    console.error("MongoDB Atlas connection error:", error);
     throw error;
   }
 
