7655ff2eaf94a7788942cd59a5277cac7a867b35
Viplav jha <viplavjha1993@gmail.com>
2024-03-10 21:31:57 +0530

    middleware

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,11 +1,10 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ['/api/webhooks/clerk'],
- 
+  publicRoutes: ["/api/webhooks/clerk"],
 });
- 
+
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
